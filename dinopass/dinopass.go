992709a37662ba1b01d2fb034a083b5178c62ee0
Ron Egli <ron@r-egli.com>
2017-04-10 11:39:59 -0700

    Create dinopass.go

---
+++ dinopass/dinopass.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+        "io/ioutil"
+        "log"
+        "net/http"
+        "fmt"
+        "github.com/atotto/clipboard"
+)
+
+func main() {
+        response, err := http.Get("http://www.dinopass.com/password/strong")
+        if err != nil {
+                log.Fatal(err)
+        } else {
+                defer response.Body.Close()
+                bodyBytes, err := ioutil.ReadAll(response.Body)
+                clipboard.WriteAll(string(bodyBytes));
+                fmt.Print(string(bodyBytes))
+                _ = err
+        }
+}
